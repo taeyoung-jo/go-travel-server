4e83e5c61dff3bdb0519dfdfb21f60fa0501971d
kkkwp <chxrryda@gmail.com>
2025-10-22 16:33:23 +0900

    feat: location 기능 구현

--- src/main/java/com/travelers/gotravelserver/domain/location/Location.java
+++ src/main/java/com/travelers/gotravelserver/domain/location/Location.java
@@ -1,17 +1,35 @@
 package com.travelers.gotravelserver.domain.location;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "locations", uniqueConstraints = @UniqueConstraint(columnNames = {"region", "city"}))
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Location {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String continent;
+
+	@Column(name = "region", nullable = false)
+	@NotBlank
+	private String region;
+
+	@Column(name = "city", nullable = false)
+	@NotBlank
 	private String city;
 }
