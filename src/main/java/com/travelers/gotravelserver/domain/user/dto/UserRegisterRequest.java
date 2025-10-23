e9a54588fdf137c8e1a7c6aee8dd3c03c660a348
kkkwp <chxrryda@gmail.com>
2025-10-23 10:00:33 +0900

    fix: 비밀번호 규칙 완화
    - 최소 8자 이상
    - 영문 1개, 숫자 1개 이상

--- src/main/java/com/travelers/gotravelserver/domain/user/dto/UserRegisterRequest.java
+++ src/main/java/com/travelers/gotravelserver/domain/user/dto/UserRegisterRequest.java
@@ -16,10 +16,10 @@ public class UserRegisterRequest {
 	private String email;
 
 	@NotBlank
-	@Size(min = 8, message = "비밀번호는 적어도 8자리 이상이어야 합니다.")
+	@Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
 	@Pattern(
-		regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-={}:;<>?,./]).*$",
-		message = "비밀번호는 숫자, 대문자, 소문자, 특수문자를 각각 최소 하나 이상 포함해야 합니다."
+		regexp = "^(?=.*[A-Za-z])(?=.*\\d).+$",
+		message = "비밀번호는 영문자와 숫자를 각각 최소 하나 이상 포함해야 합니다."
 	)
 	private String password;
 
--- src/main/java/com/travelers/gotravelserver/domain/user/dto/UserUpdateRequest.java
+++ src/main/java/com/travelers/gotravelserver/domain/user/dto/UserUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.travelers.gotravelserver.domain.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -11,10 +12,11 @@ public class UserUpdateRequest {
 
 	private String phone;
 
+	@NotBlank
 	@Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
 	@Pattern(
-		regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-={}:;<>?,./]).*$",
-		message = "비밀번호는 숫자, 대문자, 소문자, 특수문자를 각각 최소 하나 이상 포함해야 합니다."
+		regexp = "^(?=.*[A-Za-z])(?=.*\\d).+$",
+		message = "비밀번호는 영문자와 숫자를 각각 최소 하나 이상 포함해야 합니다."
 	)
 	private String password;
 }
