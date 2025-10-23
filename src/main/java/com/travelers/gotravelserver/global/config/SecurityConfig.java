70ac4436ef6164bc80f427fa2ee5e4416a30cb3d
kkkwp <chxrryda@gmail.com>
2025-10-23 15:42:54 +0900

    feat: security에서 우선 전체 경로 허용 설정

--- src/main/java/com/travelers/gotravelserver/global/config/SecurityConfig.java
+++ src/main/java/com/travelers/gotravelserver/global/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.travelers.gotravelserver.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +14,16 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable) // API 사용 시 CSRF 비활성화
+			.formLogin(AbstractHttpConfigurer::disable) // 기본 로그인 페이지 비활성화
+			.httpBasic(AbstractHttpConfigurer::disable); // 기본 인증 비활성화
+		http.authorizeHttpRequests(auth -> auth
+			.requestMatchers("/**").permitAll() // 전체 요청 허용
+		);
+		return http.build();
 	}
 }
